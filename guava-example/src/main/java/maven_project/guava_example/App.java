b909201a9837dd401e7e9bee7c48910cdfd18910
David <dmatthe@yahoo.com>
2022-08-29 10:55:55 -0400

    adding guava in eclipse-maven

--- guava-example/src/main/java/maven_project/guava_example/App.java
+++ guava-example/src/main/java/maven_project/guava_example/App.java
@@ -1,11 +1,21 @@
 package maven_project.guava_example;
 
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Multiset<String> animals = HashMultiset.create();
+		
+		animals.add("cat");
+		animals.add("cat");
+		animals.add("dog");
+		
+		animals.forEach(System.out::println);
+		
 	}
 }
