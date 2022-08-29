ddaf9ac2c5ee04300a8377f625d51c3e47459500
David <dmatthe@yahoo.com>
2022-08-29 07:56:30 -0400

    initial commit

---
+++ mavenProject/src/main/java/maven_project/mavenProject/App.java
@@ -0,0 +1,13 @@
+package maven_project.mavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Y'all!!" );
+    }
+}
